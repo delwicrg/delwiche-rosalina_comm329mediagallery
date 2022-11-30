813d7e1e727163bd3cfd9155b8567691923a6aa8
Rosalina Delwiche <delwicrg@clarkson.edu>
2022-11-29 21:54:26 -0500

    side menu

--- js/page.js
+++ js/page.js
@@ -37,3 +37,14 @@ function showSlides(n) {
 
 
 
+function navToggle() {
+  let siteNav = document.getElementById('site-nav');
+  let trigram = document.getElementById('trigram');
+  if (siteNav.className === 'nav-wrap') {
+    siteNav.classList.add('mobile');
+    trigram.classList.add('tri-mobile');
+  } else {
+    siteNav.classList = 'nav-wrap';
+    trigram.classList = 'nav-link';
+  }
+}
